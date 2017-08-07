42407e2786ba0dc157a9408655d59910fd6c0fba
jleon101 <30586809+jleon101@users.noreply.github.com>
2017-08-06 23:14:56 -0700

    Second Try

---
+++ CST-117-Inventory-Project-master/CST 117 - Inventory Project/DrinksUI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CST_117___Inventory_Project
+{
+    public partial class DrinksUI : Form
+    {
+        public DrinksUI()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuButton_Click(object sender, EventArgs e)
+        {
+            MenuUI MenuForm = new MenuUI();
+            MenuForm.Tag = this;
+            MenuForm.Show(this);
+            Hide();
+        }
+
+        private void DonutMenuButton_Click(object sender, EventArgs e)
+        {
+            MyBoxUI MyBoxForm = new MyBoxUI();
+            MyBoxForm.Tag = this;
+            MyBoxForm.Show(this);
+            Hide();
+        }
+    }
+}
---
+++ CST-117-Inventory-Project-master/CST 117 - Inventory Project/MyBox.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CST_117___Inventory_Project
+{
+    class MyBox
+    {
+        //This class connects variables/data to the UI: MyBoxUI
+
+    }
+}
