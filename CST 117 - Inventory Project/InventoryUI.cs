cbcf6c235e880e0c1a5a2f0d1669dce0f81be126
BryansGhost <bryyan94@live.com>
2017-08-06 22:43:11 -0700

    Added Employer access window (UI): +Inventory

---
+++ CST 117 - Inventory Project/InventoryUI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CST_117___Inventory_Project
+{
+    public partial class InventoryUI : Form
+    {
+        public InventoryUI()
+        {
+            InitializeComponent();
+        }
+    }
+}
